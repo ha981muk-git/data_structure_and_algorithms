5179c0411d027e24262d905fd07de018de36d7f1
ha981muk-git <ha981muk@gmail.com>
2021-11-29 14:25:11 +0100

    practise

---
+++ algorithms/algo_50.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+double hoch( double num, int exp){
+
+    double temp = 1;
+/*
+    for(int i =0;i<exp;++i){
+    temp *=num;
+    }
+*/ // Use either for-Schleife or While-Schleife , both works
+    while (exp>0)
+    {
+        temp*=num;
+        exp--;
+    }
+     while (exp<0)
+    {
+        temp*=1/num;
+        exp++;
+    }
+    
+
+    return temp;
+}
+
+int main(){
+
+   double a =  hoch(2.3,4);
+    printf("%f\n",a);
+    double b = hoch(4.0,2);
+    printf("%f\n",b);
+
+    double c = hoch(4.0,-3);
+    printf("%f\n",c);
+
+    return 0;
+}
